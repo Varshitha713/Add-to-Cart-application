e8ac163242991912016a61cf4414dc3a02a2a9ce
Macha Varshitha <machavarshithareddy@gmail.com>
2025-05-17 18:00:11 +0530

    modified html & added js code to display current types cart items to the webpage

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ const itemsInCart = ref(database, "items")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl=document.getElementById("add-btn")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click",function() {
   let inputValue=inputFieldEl.value
    push(itemsInCart, inputValue)
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`
    console.log(inputValue)
 })
